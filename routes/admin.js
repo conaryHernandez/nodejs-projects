ef9ce5a98237f05a1cee473035b3179514dd138d
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-08-22 19:52:37 -0600

    Converting HTML files to pug

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,8 @@ const products = [];
 
 router.get('/add-product', (req, res, next) => {
 	console.log('im a middleware 2');
-	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+	// res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+	res.render('add-product', {docTitle: 'Add Product'});
 });
 
 router.post('/add-product', (req, res, next) => {
