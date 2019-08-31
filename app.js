685d5cf298abdcc7e384af81a1fa4188ce579484
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-08-31 10:30:35 -0600

    added order and order item models

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 /** EJS ENGINE
 * app.set('view engine', 'ejs'); // setting global configuration, doesnt work for all template engine
@@ -76,6 +78,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, {through: CartItem });
 Product.belongsToMany(Cart, {through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through:  OrderItem });
 
 /* sync with database and creates tables, acording to models, if doesnt exists */
 sequelize
--- controllers/shop.js
+++ controllers/shop.js
@@ -102,10 +102,19 @@ exports.getCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
     const { productId } = req.body;
 
-    Product.findById(productId, product => {
-        Cart.deleteProduct(productId, product.price);
-        res.redirect('/cart');
-    });
+    req.user.getCart()
+        .then(cart => {
+            return cart.getProducts({ where: { id: productId } });
+        })
+        .then(products => {
+            const product = products[0];
+
+            return product.cartItem.destroy();
+        })
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
 };
 
 
