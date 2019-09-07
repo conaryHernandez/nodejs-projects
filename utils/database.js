74e2ed4e1b14700cd15c2dd355f4969b759a00ab
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-09-07 15:11:31 -0600

    storing multiple products in cart

--- utils/database.js
+++ utils/database.js
@@ -1,17 +1,19 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+// const uri = 'mongodb+srv://conaryh:k9X9MpdWnfHYcqMC@cluster0-nvbxl.mongodb.net/nodejs-sandbox?retryWrites=true&w=majority';
+const uri = "mongodb://conaryh:k9X9MpdWnfHYcqMC@cluster0-shard-00-00-nvbxl.mongodb.net:27017,cluster0-shard-00-01-nvbxl.mongodb.net:27017,cluster0-shard-00-02-nvbxl.mongodb.net:27017/nodejs-sandbox?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 let _db = null;
 
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://conaryh:k9X9MpdWnfHYcqMC@cluster0-nvbxl.mongodb.net/nodejs-sandbox?retryWrites=true&w=majority')
+    MongoClient.connect(uri,  {useNewUrlParser: true })
     .then(client => {
         console.log('connected!!!');
         _db = client.db('nodejs-sandbox'); // you can override db name here
         callback();
     })
     .catch(err => {
-        console.log(err)
+        console.log('database connection err', err)
         throw err;
     });
 };
