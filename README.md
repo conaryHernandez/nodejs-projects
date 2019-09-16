db0dca6e4183ba391b066ae9d1ace7a08d0fa5d4
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-09-15 20:01:46 -0600

    added console log for debugging

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nodejs-sandbox
 Learning Node, using authentication, pagination, APIs and much more.
+
+This is a work in progress.
--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use(flash());
 
 // retreiving mongoose object for individual request
 app.use((req, res, next) => {
+    console.log(req.session);
     if (!req.session.user) {
         return next();
     }
