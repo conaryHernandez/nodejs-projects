fe64356c06bcbfc5a166c6e293030e683b43efec
conaryh <conaryh@gmail.com>
2019-09-18 08:00:11 -0600

    Serving Images Statically and files

--- routes/shop.js
+++ routes/shop.js
@@ -17,6 +17,8 @@ router.get('/cart', isAuth, shopController.getCart);
 
 router.get('/orders', isAuth, shopController.getOrders);
 
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+
 router.post('/cart', isAuth, shopController.postCart);
 
 router.post('/cart-delete-product', isAuth, shopController.postCartDeleteProduct);
