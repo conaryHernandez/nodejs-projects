8724a9898f69c54366984505601202b6fea3105a
conaryh <conaryh@gmail.com>
2019-09-18 14:47:41 -0600

    using streams

--- controllers/shop.js
+++ controllers/shop.js
@@ -201,17 +201,27 @@ exports.getInvoice = (req, res, next) => {
     const invoiceName = `invoice-${orderId}.pdf`;
     const invoicePath = path.join('data', 'invoices', invoiceName);
 
-    fs.readFile(invoicePath, (err, data) => {
-        if (err) {
-            return next(err);
-        }
+    Order.findById({ orderId })
+        .then(order => {
+            if (!order) {
+                return next(
+                    new Error('No Order Found'));
+            }
 
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader(
-            'Content-Disposition',
-            'inline; filename="' + invoiceName + '"'
-        );
-        res.send(data);
-    });
+            if (order.user.userId.toString() !== req.user._id.toString()) {
+                return next(new Error('Not Authorized'));
+            }
 
+            const file = fs.createReadStream(invoicePath);
+
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader(
+                'Content-Disposition',
+                'inline; filename="' + invoiceName + '"'
+            );
+            file.pipe(res);
+        })
+        .catch(err => {
+            next(err);
+        });
 };
