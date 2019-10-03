611f3af97ba900be0f889a9450f5730ac917841f
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-10-03 08:45:44 -0600

    added morgan, compression and helmet, preparing for deployment

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Learning Node, using authentication, pagination, APIs and much more.
 
 This is a work in progress.
+
+Pending:
+-Set compression according to env
+-Set morgan according to env
+-Set env variables properly
+-Set SSL
+ 
--- app.js
+++ app.js
@@ -1,21 +1,46 @@
+// CORE MODULES
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
+// UTILS
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const flash = require('connect-flash');
+
+// EXPRESS
 const express = require('express');
 const expressHbs = require('express-handlebars');
+const app = express();
+
+// VARS
 require('dotenv').config()
 
-const app = express();
+// DATABASE
 const mongoose = require('mongoose');
+const MONGODB_URI = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00-nvbxl.mongodb.net:27017,cluster0-shard-00-01-nvbxl.mongodb.net:27017,cluster0-shard-00-02-nvbxl.mongodb.net:27017/${process.env.MONGO_DEFAULT_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&w=majority`;
+
+// SESSIONS
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
-const flash = require('connect-flash');
+
+// PROD Dependencies
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+
+// STORING SESSIONS
 const store = new MongoDBStore({
-    uri: process.env.MONGODB_URI,
+    uri: MONGODB_URI,
     collection: 'sessions'
 });
 const csrfProtection = csrf();
+
+// SSL
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
+
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'images');
@@ -36,14 +61,8 @@ const fileFilter = (req, file, cb) => {
         cb(null, false);
     }
 };
-/** EJS ENGINE
- * app.set('view engine', 'ejs'); // setting global configuration, doesnt work for all template engine
- * app.set('views', 'views');
- */
 
-/** HANDLEBARS ENGINE
- * .engine is used to specify an engine that is not build in with express
- */
+// HANDLEBARS ENGINE
 const hbs = expressHbs.create({
     layoutsDir: 'views/layouts/', // only use this when you have other directory name.
     partialsDir: 'views/partials/',
@@ -60,22 +79,26 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars'); // setting global configuration, doesnt work for all template engine
 app.set('views', 'views');
 
-/* PUG ENGINE
-	app.set('view engine', 'pug'); // setting global configuration, doesnt work for all template engine
-	app.set('views', 'views');
-*/
-
+// IMPORTING ROUTES
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+
+// Logging
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+
+// Prod setup
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', {stream: accessLogStream}));
+
+// CONTROLLERS
 const errorController = require('./controllers/404');
 const User = require('./models/user');
 const shopController = require('./controllers/shop');
 const isAuth = require('./middleware/is-auth');
 
-/* parsing the body
- * Urlencoded in the end call next but before that it parses the body, does not parse all type of bodies
- */
+// UTILS middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -88,6 +111,7 @@ app.use(session({
     store,
     // cookie: {}
 }));
+
 app.use(flash());
 
 app.use((req, res, next) => {
@@ -95,7 +119,7 @@ app.use((req, res, next) => {
     next();
 });
 
-// retreiving mongoose object for individual request
+// USER AUTH
 app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
@@ -105,8 +129,7 @@ app.use((req, res, next) => {
             if (!user) {
                 return next();
             }
-            req.user = user;
-            // res.setHeader('Set-Cookie', 'isLoggedIn=true');
+            req.user = user; // retreiving mongoose object for individual request
             next();
         })
         .catch(err => {
@@ -130,12 +153,10 @@ app.use(authRoutes);
 
 app.use('/error', errorController.get500);
 
-/**	404 PAGE */
+/**	ERRORS */
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    // res.status(error.httpStatusCode);
-    // res.redirect('/error');
     console.log(error);
     res.status(500).render('500', {
         pageTitle: 'Error',
@@ -144,11 +165,12 @@ app.use((error, req, res, next) => {
     });
 });
 
-
+// DATABASE CONNECTION
 mongoose
-    .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+    .connect(MONGODB_URI, { useNewUrlParser: true })
     .then(result => {
-        app.listen(3000);
+        app.listen(process.env.PORT || 3000);
+        // https.createServer({key: privateKey, cert: certificate}, app).listen(process.env.PORT || 3000);
     })
     .catch(err => {
         console.log(err);
