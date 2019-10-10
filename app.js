c24a7babde557391fe526bbeaa182ef5769bfecc
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-10-09 19:58:33 -0600

    testing jenkins

--- app.js
+++ app.js
@@ -88,11 +88,9 @@ const authRoutes = require('./routes/auth');
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
 
 // Prod setup
-if (process.env.ENVIRONMENT !== 'dev') {
-    app.use(helmet());
-    app.use(compression());
-    app.use(morgan('combined', {stream: accessLogStream}));
-}
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', {stream: accessLogStream}));
 
 // CONTROLLERS
 const errorController = require('./controllers/404');
